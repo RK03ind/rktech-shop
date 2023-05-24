fe6e31a793ff5f83e8720f932fde4d86b1f5f6d8
RK03ind <gamitech95@gmail.com>
2023-05-24 23:04:26 +0530

    UPDATE: get data hook for sending jwt token

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-const useGetData = (url) => {
+const useGetData = (url, isAuthRequired = false) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, {
+          headers: {
+            authorization: isAuthRequired ? authCtx.token : "",
+          },
+        });
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
@@ -20,7 +26,6 @@ const useGetData = (url) => {
         setLoading(false);
       }
     };
-
     fetchData();
   }, [url]);
 
