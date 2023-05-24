45bb78f41d5e4d2e6640a6822822607c949c0b76
RK03ind <gamitech95@gmail.com>
2023-05-24 23:08:38 +0530

    ADD: Function and hooks to product card for adding and removing from cart and wishlist

--- src/shared/ProductCard/ProductCard.jsx
+++ src/shared/ProductCard/ProductCard.jsx
@@ -1,7 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BsFillCartCheckFill, BsFillCartPlusFill } from "react-icons/bs";
 import { AiOutlineHeart, AiFillHeart, AiFillStar } from "react-icons/ai";
 import "./ProductCard.css";
-const ProductCard = ({ imageLink, rating, price, name }) => {
+import usePostData from "../../hooks/usePostData";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import useDeleteData from "../../hooks/useDeleteData";
+
+const ProductCard = ({ imageLink, rating, price, name, _id }) => {
+  const addToCart = usePostData("/api/user/cart", true);
+  const addToWishlist = usePostData("/api/user/wishlist", true);
+  const removeFromCart = useDeleteData(`/api/user/cart/${_id}`, true);
+  const removeFromWishlist = useDeleteData(`/api/user/wishlist/${_id}`, true);
+  const authCtx = useContext(AuthContext);
+
+  const addToCartHandler = () => {
+    addToCart.postData({ product: { imageLink, rating, price, name, _id } });
+  };
+
+  const addToWishlistHandler = () => {
+    addToWishlist.postData({
+      product: { imageLink, rating, price, name, _id },
+    });
+  };
+
   return (
     <div className="product-card">
       <img src={imageLink} alt={name} />
@@ -12,10 +34,18 @@ const ProductCard = ({ imageLink, rating, price, name }) => {
       </div>
       <div className="product-action">
         <div>
-          <BsFillCartPlusFill />
+          {authCtx.userData.cart.some((item) => item._id === _id) ? (
+            <BsFillCartCheckFill onClick={() => removeFromCart.deleteData()} />
+          ) : (
+            <BsFillCartPlusFill onClick={addToCartHandler} />
+          )}
         </div>
         <div>
-          <AiOutlineHeart />
+          {authCtx.userData.wishlist.some((item) => item._id === _id) ? (
+            <AiFillHeart onClick={() => removeFromWishlist.deleteData()} />
+          ) : (
+            <AiOutlineHeart onClick={addToWishlistHandler} />
+          )}
         </div>
         <div className="rating">
           {rating}
