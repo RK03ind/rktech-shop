3facf0f61e5530686fdfc200679ab7b71e491670
RK03ind <gamitech95@gmail.com>
2023-05-31 13:33:35 +0530

    UPDATED: State Handling of checkout cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -8,10 +8,26 @@ const Cart = () => {
       userData: { cart },
     },
   } = useContext(AuthContext);
-  const [totalAmount, setTotalAmount] = useState(0);
-
+  const [checkoutDataState, setCheckoutDataState] = useState({
+    totalAmount: 0,
+    delivery: 100,
+    discount: 0,
+    initialPrice: 0,
+    noOfItems: 0,
+  });
   useEffect(() => {
-    setTotalAmount(cart.reduce((acc, { price, qty }) => acc + price * qty, 0));
+    let initialPrice = cart.reduce(
+      (acc, { price, qty }) => acc + price * qty,
+      0
+    );
+    setCheckoutDataState((prevState) => {
+      return {
+        ...prevState,
+        totalAmount: initialPrice - prevState.discount + prevState.delivery,
+        initialPrice,
+        noOfItems: cart.length,
+      };
+    });
   }, [cart]);
 
   return (
@@ -19,15 +35,40 @@ const Cart = () => {
       <div className="cart-container">
         <h2>My Cart</h2>
         <div className="cart-items">
-          <div className="cart-item-wrapper">
-            {cart.map((item) => {
-              return <CartItem {...item} />;
-            })}
-          </div>
-          <div className="checkout-card">
-            <h4>PRICE DETAILS</h4>
-            <div>{totalAmount}</div>
-          </div>
+          {cart.length === 0 ? (
+            <h1>No Items in Cart 😟</h1>
+          ) : (
+            <>
+              <div className="cart-item-wrapper">
+                {cart.map((item) => {
+                  return <CartItem {...item} />;
+                })}
+              </div>
+
+              <div className="checkout-card">
+                <div className="price-details">
+                  <h4>PRICE DETAILS</h4>
+                  <div className="price-item">
+                    <span>Price({checkoutDataState.noOfItems})</span>
+                    <span>₹{checkoutDataState.initialPrice}</span>
+                  </div>
+                  <div className="price-item">
+                    <span>Discount</span>
+                    <span>- ₹{checkoutDataState.discount}</span>
+                  </div>
+                  <div className="price-item">
+                    <span>Delivery Charges</span>
+                    <span>+ ₹{checkoutDataState.delivery}</span>
+                  </div>
+                  <div className="price-item final">
+                    <span>Total Amount</span>
+                    <span>₹{checkoutDataState.totalAmount}</span>
+                  </div>
+                </div>
+                <button>Checkout</button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
