56b9d90f81fd387785066ab1396fa7edcdcf0296
RK03ind <gamitech95@gmail.com>
2023-05-31 15:53:10 +0530

    ADD: Mobile nav with toggle logic

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,20 +1,31 @@
 import { ImSearch } from "react-icons/im";
 import { FaShoppingBag, FaShoppingCart, FaUser, FaHeart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FilterMenuContext } from "../../context/FilterMenuContext";
 const Navbar = () => {
   const filterMenuCtx = useContext(FilterMenuContext);
+  const [isMenuVisible, setMenuVisibility] = useState(false);
   const navigate = useNavigate();
+
+  const toggleMobileMenu = () => {
+    setMenuVisibility((prevState) => {
+      return !prevState;
+    });
+  };
   return (
     <>
       <nav>
         <span className="title" onClick={() => navigate("/")}>
           💻RKTECH
         </span>
-        <div className="nav-tabs">
+        <div
+          className={`nav-tabs ${isMenuVisible ? "active" : ""}`}
+          onClick={() => setMenuVisibility(false)}
+        >
           <ImSearch
             onClick={() => filterMenuCtx.dispatch({ type: "TOGGLE_SEARCH" })}
           />
@@ -23,6 +34,7 @@ const Navbar = () => {
           <FaHeart onClick={() => navigate("wishlist")} />
           <FaUser onClick={() => navigate("profile")} />
         </div>
+        <GiHamburgerMenu className="mobile-menu" onClick={toggleMobileMenu} />
       </nav>
       {filterMenuCtx.state.isSearchVisible && <SearchBar />}
     </>
