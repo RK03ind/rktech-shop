964e77948180f6b6aea0724b39f8d1ef5f037238
RK03ind <gamitech95@gmail.com>
2023-06-02 00:45:46 +0530

    ADD: Product page component

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,35 @@
+import { useParams } from "react-router-dom";
+import useGetData from "../../hooks/useGetData";
+import { AiOutlineHeart, AiFillHeart, AiFillStar } from "react-icons/ai";
 import "./styles/Product.css";
 const Product = () => {
+  const { productId } = useParams();
+  console.log(productId);
+  // const { data, loading, error } = useGetData(
+  //   "/api/user/products/:productId",
+  //   false
+  // );
   return (
     <>
-      <div className="product"></div>
+      <div className="product">
+        <img
+          src="https://res.cloudinary.com/rk03/image/upload/v1684792155/rktech/geforce-rtx-3090-ti-suprim-x-24g-1-550x550_wmywv5.jpg"
+          alt=""
+        />
+        <div className="product-details">
+          <div className="title">RTX 3090TI SUPRIM</div>
+          <div className="price">₹100000</div>
+          <div className="rating">
+            4.6 <AiFillStar />
+          </div>
+          <div className="label">Product Model: 12102X0P</div>
+          <div className="label">Category: GPU</div>
+          <div className="action-buttons">
+            <button>Add To Cart</button>
+            <button>Add To Wishlist</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
