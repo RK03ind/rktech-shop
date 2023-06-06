66c8320534eaf15fa08f21ab8b8a6e78440a5690
RK03ind <gamitech95@gmail.com>
2023-06-07 00:01:31 +0530

    ADD: Logic to filter category on click

--- src/pages/Home/components/CategoryItem.jsx
+++ src/pages/Home/components/CategoryItem.jsx
@@ -1,6 +1,17 @@
+import { useContext } from "react";
+import { FilterMenuContext } from "../../../context/FilterMenuContext";
+import { useNavigate } from "react-router-dom";
+
 const CategoryItem = ({ icon, categoryName }) => {
+  const { dispatch } = useContext(FilterMenuContext);
+  const navigate = useNavigate();
+  const navToCategory = () => {
+    dispatch({ type: "UPDATE_CATEGORY", payload: categoryName });
+    navigate("products");
+  };
+
   return (
-    <div className="category-item">
+    <div className="category-item" onClick={navToCategory}>
       <img src={icon} alt={categoryName} />
       <span>{categoryName}</span>
     </div>
